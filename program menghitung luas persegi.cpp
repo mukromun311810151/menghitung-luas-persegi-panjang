07887ce99dd606c10ecf62bf649583d111191242
mukromun 311810151 <45851430+mukromun311810151@users.noreply.github.com>
2019-01-01 21:34:45 +0700

    Add files via upload

---
+++ program menghitung luas persegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int panjang, lebar, luas;
+	
+	cout << "masukkan panjang : ";
+	cin >> panjang;
+	
+	cout << "masukkan lebar : ";
+	cin >> lebar;
+	
+	luas = panjang * lebar;
+	cout << "luas persegi panjang : " <<luas;
+	return 0;
+	
+	
+}
